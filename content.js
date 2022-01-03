b83d728b9d10a564103cae9e40017c6456a1d408
Balagardash <azbalagardashbashirov@gmail.com>
2022-01-03 15:12:44 +0000

    Update content.js
    update due to tw changing DOM stuff

--- content.js
+++ content.js
@@ -1,47 +1,10 @@
-const SELECTORS = {
-  "tweetButton": "div[data-testid='tweetButtonInline'] > div > span > span",
-  "retweetButton": "div[data-testid='retweet']",
-  "quoteButton": "#layers > div.css-1dbjc4n.r-1p0dtai.r-1d2f490.r-105ug2t.r-u8s1d.r-zchlnj.r-ipm5af > div > div > div > div:nth-child(2) > div.css-1dbjc4n.r-14lw9ot.r-z2wwpe.r-1upvrn0.r-j2cz3j.r-1udh08x.r-u8s1d > div > div > div > a > div.css-1dbjc4n.r-16y2uox.r-1wbh5a2 > div > span",
-  "retweetButton1": "#layers > div.css-1dbjc4n.r-1p0dtai.r-1d2f490.r-105ug2t.r-u8s1d.r-zchlnj.r-ipm5af > div > div > div > div:nth-child(2) > div.css-1dbjc4n.r-14lw9ot.r-z2wwpe.r-1upvrn0.r-j2cz3j.r-1udh08x.r-u8s1d > div > div > div > div > div.css-1dbjc4n.r-16y2uox.r-1wbh5a2 > div > span"
-}
-
-window.addEventListener("load", (evt) => {
-  waitForElement(SELECTORS["tweetButton"], function(b) { b.innerHTML = "Cükküldə" })
-  waitForElement(SELECTORS["quoteButton"], function(b) { b.innerHTML = "Cükkültünü Linçlə" })
-}, false)
-
-function waitForElement(query, callback) {
-  var counter = 0
-  var el = document.querySelector(query)
-    //while ((!el) & (counter < 50)) {
-    //setTimeout(() => {
-    //el = document.querySelector(query);
-    //console.log(counter)
-    //counter++
-    //}, 100)
-    //}
-  setTimeout(() => {
-    el = document.querySelector(query)
-    if (el) { callback(el) } else {
-      console.log(el)
+setInterval(()=>{
+    document.querySelector("div[data-testid='tweetButtonInline'] span > span").innerText = "Cükküldə"
+    document.querySelector("div[data-testid='retweet']").onclick = () => {
+      setTimeout(()=>{
+        rtC = document.querySelector("div[data-testid='retweetConfirm']")
+        rtC.querySelector("span").innerText = "Cükküldət"
+        rtC.parentElement.querySelector("a span").innerText = "Cükkültünü linçlə"
+      },100)
     }
-  }, 2000)
-}
-
-// Since tweets load dynamically we will call this every 2 seconds
-
-setInterval(() => {
-  var rtButtons = document.querySelectorAll(SELECTORS["retweetButton"])
-  console.log(rtButtons)
-  rtButtons.forEach((bt) => {
-    bt.onclick = () => {
-      console.log("YO YO")
-      setTimeout(() => {
-        qb = document.querySelector(SELECTORS["quoteButton"])
-        qb.innerHTML = "Cükkültünü Linçlə"
-        rb = document.querySelector(SELECTORS["retweetButton1"])
-        rb.innerHTML = "Cükküldətdir"
-      }, 100)
-    }
-  })
-}, 2000)
+}, 1000)
