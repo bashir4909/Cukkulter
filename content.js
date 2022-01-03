3b32fd8198d2b589645f89991e835b0fbcd50d89
Balagardash <azbalagardashbashirov@gmail.com>
2022-01-03 15:33:57 +0000

    Update content.js
    bunch of chanhges to renaming

--- content.js
+++ content.js
@@ -1,10 +1,37 @@
-setInterval(()=>{
-    document.querySelector("div[data-testid='tweetButtonInline'] span > span").innerText = "Cükküldə"
-    document.querySelector("div[data-testid='retweet']").onclick = () => {
-      setTimeout(()=>{
-        rtC = document.querySelector("div[data-testid='retweetConfirm']")
-        rtC.querySelector("span").innerText = "Cükküldət"
-        rtC.parentElement.querySelector("a span").innerText = "Cükkültünü linçlə"
-      },100)
+function dothething(){
+    twb = document.querySelector("div[data-testid='tweetButtonInline'] span > span")
+    if (twb) {
+      
+    if (twb.innerText==="Tweet")
+    {
+      twb.innerText = "Cükküldə"
+    } else if (twb.innerText==="Reply")
+    {
+      twb.innerText = "Cükküldəş"
     }
-}, 1000)
+  }
+    twb = document.querySelector("div[data-testid='tweetButton'] span > span")
+    if(twb){
+    if (twb.innerText==="Tweet")
+    {
+      twb.innerText = "Cükküldə"
+    } else if (twb.innerText==="Reply")
+    {
+      twb.innerText = "Cükküldəş"
+    }}
+    rtb = document.querySelector("div[data-testid='retweet']")
+    if (rtb) {
+      document.querySelector("div[data-testid='retweet']").onclick = () => {
+        setTimeout(()=>{
+          rtC = document.querySelector("div[data-testid='retweetConfirm']")
+          rtC.querySelector("span").innerText = "Cükküldət"
+          rtC.parentElement.querySelector("a span").innerText = "Linçlə"
+        },100)
+      }  
+    }
+    
+  }
+
+window.addEventListener('load',()=>{
+  setInterval(dothething, 1000)
+})
